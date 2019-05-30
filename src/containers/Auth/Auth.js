d8e060312ba921618dd7b8a5aca1213956fb24b6
Lutsevich Dzianis <dzianis.lutsevich@altoros.corp>
2019-05-30 16:54:39 +0300

    Complete 364 lecture.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.module.css';
 import * as actions from '../../store/actions/index';
 
@@ -37,8 +39,15 @@ class Auth extends Component {
                     minLength: 6
                 }
             }
-        }
+        },
+        isSignup: true
     };
+
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
+    }
 
     inputChangedHandler = (event, controlName) => {
         const updatedControls = {
@@ -56,7 +65,13 @@ class Auth extends Component {
 
     submitHandler = event => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+    };
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return { isSignup: !prevState.isSignup };
+        });
     };
 
     checkValidity(value, rules) {
@@ -87,7 +102,7 @@ class Auth extends Component {
             });
         }
 
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input
                 key={ formElement.id }
                 elementType={ formElement.config.elementType }
@@ -99,21 +114,55 @@ class Auth extends Component {
                 changed={ event => this.inputChangedHandler(event, formElement.id) } />
         ));
 
+        if (this.props.loading) {
+            form = <Spinner />;
+        }
+
+        let errorMessage = null;
+
+        if (this.props.error) {
+            errorMessage = (
+                <p>{ this.props.error.message }</p>
+            );
+        }
+
+        let authRedirect = null;
+        if (this.props.isAuthenticated) {
+            authRedirect = <Redirect to={ this.props.authRedirectPath } />;
+        }
+
         return (
             <div className={ classes.Auth }>
+                { authRedirect }
+                { errorMessage }
                 <form onSubmit={ this.submitHandler }>
                     { form }
                     <Button btnType='Success'>SUBMIT</Button>
                 </form>
+                <Button
+                    clicked={ this.switchAuthModeHandler }
+                    btnType='Danger'>SWITCH TO { this.state.isSignup ? 'SIGNIN' : 'SIGNUP' }
+                </Button>
             </div>
         );
     }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = state => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        loading: state.auth.loading,
+        error: state.auth.error,
+        isAuthenticated: state.auth.token != null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -20,8 +23,85 @@ export const authFail = error => {
     };
 };
 
-export const auth = (email, password) => {
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
+
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = expirationTime => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
+
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBz2eoSFLIUx2DQm6kOvOU9RT-KDnoetJw';
+
+        if (!isSignup) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBz2eoSFLIUx2DQm6kOvOU9RT-KDnoetJw';
+        }
+
+        axios.post(url, authData)
+            .then(
+                response => {
+                    const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+
+                    localStorage.setItem('token', response.data.idToken);
+                    localStorage.setItem('expirationDate', expirationDate);
+                    localStorage.setItem('userId', response.data.localId);
+
+                    dispatch(authSuccess(response.data.idToken, response.data.localId));
+                    dispatch(checkAuthTimeout(response.data.expiresIn));
+                }
+            )
+            .catch(
+                err => {
+                    dispatch(authFail(err.response.data.error));
+                }
+            );
+    };
+};
+
+export const setAuthRedirectPath = path => {
+    return {
+        type: actionTypes.SET_AUTH_REDIRECT_PATH,
+        path: path
+    };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+
+            if (expirationDate > new Date()) {
+                const userId = localStorage.getItem('userId');
+
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+            } else {
+                dispatch(logout());
+            }
+        }
     };
 };
